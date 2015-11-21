23bee306c428407e9b5886040ae76d21f85c6489
aleju <kontakt@ajung.name>
2015-11-21 19:14:48 +0100

    Update to keyword based downloading

--- dataset/downloader.py
+++ dataset/downloader.py
@@ -1,78 +1,132 @@
+"""File to download example sky images from flickr."""
+from __future__ import print_function, division
 import socket
-socket.setdefaulttimeout(5)
+socket.setdefaulttimeout(10)
 import urllib
 import os
 import time
 import re
 import traceback
 
-MAIN_URLS = [
-    "https://www.flickr.com/search/?text=sky&media=photos&page=%PAGE&view_all=1",
-    "https://www.flickr.com/search/?text=night%20sky&media=photos&page=%PAGE&view_all=1",
-    "https://www.flickr.com/search/?text=sunset&media=photos&page=%PAGE&view_all=1",
-    "https://www.flickr.com/search/?text=sunrise&media=photos&page=%PAGE&view_all=1"
-]
-DEST_DIR = "orig"
-URLS_LIST_FILEPATH = "_urls.txt"
+FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+WRITE_TO_BASE_DIR = os.path.join(FILE_DIR, "downloaded")
+BASE_URL = "https://www.flickr.com/search/?text=%KEYWORD&media=photos&page=%PAGE&view_all=1"
+KEYWORDS = ["sky", "night%20sky", "sunset", "sunrise"]
+URLS_LIST_FILEPATH = os.path.join(WRITE_TO_BASE_DIR, "_urls.txt")
 PATTERN = re.compile(r"_z\.jpg$")
 
 def main():
-    with open(URLS_LIST_FILEPATH, "a") as fhandle:
+    """Main function: Iterates over search pages and downloads images linked on them."""
+    with open(URLS_LIST_FILEPATH, "a") as furl:
         # seems to be 25 images per page
-        for page_no in range(1, 10000):
+        # by default: scan pages 1 to 250 per keyword
+        for page_no in range(1, 250):
             print("<Page> %d" % (page_no))
-            for main_url in MAIN_URLS:
-                source = load_page_source(main_url, page_no)
-                raw_urls = extract_image_urls(source)
-                urls = set([fix_url(url) for url in raw_urls])
-                for url in urls:
-                    # Examples:
-                    # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d.jpg   (500x333, 1.5:1)
-                    # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d_n.jpg (320x213, 1.5:1, 64%)
-                    # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d_q.jpg (150x150, 1.0:1, 30%)
-                    # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d_m.jpg (240x160, 1.5:1, 48%)
-                    # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d_s.jpg ( 75x75,  1.0:1, 15%)
-                    # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d_t.jpg (100x67,  1.5:1, 20%)
-                    # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d_z.jpg (640x427, 1.5:1, 128%)
-                    #if "_n" in url or "_t" in url:
-                    if PATTERN.search(url):
-                        print("<Image> %s" % (url))
-                        try:
-                            download_image(url, DEST_DIR, fhandle)
-                        except Exception as e:
-                            traceback.print_exc()
-                            print(e)
-                        time.sleep(1.0)
+            for keyword in KEYWORDS:
+                main_url = BASE_URL.replace("%KEYWORD", keyword)
+                dest_dir = os.path.join(WRITE_TO_BASE_DIR, "%s/" % (keyword,))
+                try:
+                    source = load_page_source(main_url, page_no)
+                except Exception as exc:
+                    traceback.print_exc()
+                    print(exc)
+                    source = None
+
+                if source is not None:
+                    raw_urls = extract_image_urls(source)
+                    urls = set([fix_url(url) for url in raw_urls])
+                    for url in urls:
+                        # Examples:
+                        # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d.jpg   (500x333, 1.5:1)
+                        # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d_n.jpg (320x213, 1.5:1, 64%)
+                        # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d_q.jpg (150x150, 1.0:1, 30%)
+                        # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d_m.jpg (240x160, 1.5:1, 48%)
+                        # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d_s.jpg ( 75x75,  1.0:1, 15%)
+                        # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d_t.jpg (100x67,  1.5:1, 20%)
+                        # http://c3.staticflickr.com/3/2935/14030724164_8a4cf0b48d_z.jpg (640x427, 1.5:1, 128%)
+                        if PATTERN.search(url):
+                            print("<Image> %s" % (url))
+                            try:
+                                downloaded = download_image(url, dest_dir, furl)
+                                if downloaded:
+                                    # wait for 1sec before downloading the next image
+                                    time.sleep(1.0)
+                            except Exception as exc:
+                                traceback.print_exc()
+                                print(exc)
+
+                # wait for 5secs before opening the next search page
                 time.sleep(5.0)
 
 def load_page_source(main_url, page_no):
+    """Load the source code of a flickr search page.
+    Args:
+        main_url The search page url, must contain "%PAGE"
+        page_no The number of the search page to request (1 ... N)
+    Returns:
+        Html content
+    """
     url = main_url.replace("%PAGE", str(page_no))
     return " ".join(urllib.urlopen(url).readlines())
 
 def extract_image_urls(source):
+    """Finds urls to images in the source code of a flickr search page.
+    Args:
+        source HTML-Sourc code
+    Returns:
+        List of urls (strings)
+    """
+    # todo pylint complains here
     source = source.replace("\/", "/")
     pattern = re.compile(r"\/\/[a-zA-Z0-9]{1,4}\.staticflickr\.com\/[a-zA-Z0-9\/_]+\.(?:jpg|jpeg|png)")
     matches = re.findall(pattern, source)
     return matches
 
 def fix_url(url):
+    """Makes sure that an url is properly formatted.
+    Args:
+        url The url to fix
+    Returns:
+        The fixed URL
+    """
     if url.startswith("//"):
         return "http:" + url
     else:
         return url
 
 def download_image(source_url, dest_dir, urls_list_file):
+    """Downloads an image from flickr and saves it.
+    Images that were already downloaded are skipped automatically.
+    
+    Args:
+        source_url The URL of the image.
+        dest_dir The directory to save the image in.
+        urls_list_file File handle for the file in which the URLs of downloaded images will be
+                       saved.
+    Returns:
+        True if the image was downloaded
+        False otherwise (including images that were skipped)
+    """
     if "/" not in source_url or (".jpg" not in source_url and ".jpeg" not in source_url) or "?" in source_url:
         print("[Warning] source url '%s' is invalid" % (source_url))
+        return False
     else:
         index = source_url.rfind(".com/")
         image_name = source_url[index+len(".com/"):].replace("/", "-")
         filepath = os.path.join(dest_dir, image_name)
         if os.path.isfile(filepath):
             print("[Info] skipped '%s', already downloaded" % (filepath))
+            return False
         else:
-            urls_list_file.write("%s\t%s\n" % (source_url, image_name))
+            # create directory if it doesnt exist
+            if not os.path.exists(dest_dir):
+                os.makedirs(dest_dir)
+
+            # add "<URL>\t<Image-Filepath>" (without <, >) to urls file
+            urls_list_file.write("%s\t%s\n" % (source_url, filepath))
+            # download the image
             urllib.urlretrieve(source_url, filepath)
+            return True
 
 if __name__ == "__main__":
     main()
--- dataset/generate_dataset.py
+++ dataset/generate_dataset.py
@@ -1,7 +1,7 @@
+"""Shrinks and augments downloaded images in order to create a training set."""
 from __future__ import print_function, division
 import os
 import re
-import math
 import random
 import numpy as np
 from scipy import misc, ndimage
@@ -12,8 +12,8 @@ random.seed(42)
 KEYWORDS = ["sky", "night%20sky", "sunset", "sunrise"]
 
 MAIN_DIR = os.path.dirname(os.path.realpath(__file__))
-#READ_MAIN_DIR = os.path.join(MAIN_DIR, "downloaded/")
-READ_MAIN_DIR = "/media/aj/grab/ml/datasets/flickr-sky"
+READ_MAIN_DIR = os.path.join(MAIN_DIR, "downloaded/")
+#READ_MAIN_DIR = "/media/aj/grab/ml/datasets/flickr-sky"
 WRITE_MAIN_DIR = os.path.join(MAIN_DIR, "preprocessed/")
 DIRS = [(os.path.join(READ_MAIN_DIR, "%s/" % (keyword,)), \
          os.path.join(WRITE_MAIN_DIR, "%s/" % (keyword,))) for keyword in KEYWORDS]
@@ -22,48 +22,56 @@ SCALE_HEIGHT = 32
 SCALE_WIDTH = 64
 RATIO_WIDTH_TO_HEIGHT = 2
 EPSILON = 0.1
-#WRITE_TO = os.path.join(MAIN_DIR, "out_aug_32x64")
 
 AUGMENTATIONS = 10
 PADDING = 20
 
 def main():
+    """Iterates over the images in each directory, shrinks and augments each one."""
     nb_processed = 0
     nb_errors = 0
     nb_total = len(get_all_filepaths([download_dir for download_dir, write_to_dir in DIRS]))
-    
+
+    # iterate over directories (read-directory and save-to-directory)
     for download_dir, write_to_dir in DIRS:
         print("Reading from '%s'" % (download_dir,))
         print("Writing to '%s'" % (write_to_dir,))
-        
+
         # create directory if it doesnt exist
         if not os.path.exists(write_to_dir):
             os.makedirs(write_to_dir)
-        
+
+        # load filepaths of images in directory
         fps_img = get_all_filepaths([download_dir])
-        
-        for fp_idx, fp_img in enumerate(fps_img):
+
+        # iterate over each image
+        for fp_img in fps_img:
             print("Image %d of %d (%.2f%%) (%s)" \
                   % (nb_processed+1, nb_total, 100*(nb_processed+1)/nb_total, fp_img))
             try:
                 filename = fp_img[fp_img.rfind("/")+1:]
+
+                # dont use misc.imload, fails for grayscale images
                 image = ndimage.imread(fp_img, mode="RGB")
                 image_orig = np.copy(image)
                 #misc.imshow(image)
                 #print(image)
                 #print(image.shape)
-                
+
                 height = image_orig.shape[0]
                 width = image_orig.shape[1]
                 wh_ratio = width / height
+
+                # add padding at the borders of the image
+                # then augment image
                 batch = np.zeros((AUGMENTATIONS, height+(2*PADDING), width+(2*PADDING), 3),
                                  dtype=np.uint8)
-                
+
                 img_padded = np.pad(image, ((PADDING, PADDING), (PADDING, PADDING), (0, 0)),
                                     mode="median")
                 for i in range(0, AUGMENTATIONS):
                     batch[i] = np.copy(img_padded)
-                
+
                 ia = ImageAugmenter(width+(2*PADDING), height+(2*PADDING),
                                     channel_is_first_axis=False,
                                     hflip=True, vflip=False,
@@ -71,10 +79,16 @@ def main():
                                     rotation_deg=5, shear_deg=1,
                                     translation_x_px=15, translation_y_px=15)
                 batch = ia.augment_batch(batch)
-                
+
                 for i in range(0, AUGMENTATIONS):
+                    # remove padding
                     image = batch[i, PADDING:-PADDING, PADDING:-PADDING, ...]
-                    
+
+                    # shrink the image to desired height/width sizes
+                    # first delete rows/columns until aspect ratio matches desired aspect ratio
+                    # then resize
+                    # doing this after the augmentation should decrease the likelihood of
+                    # ending with badly looking black areas at the borders of the image
                     removed = 0
                     while not (wh_ratio - EPSILON <= RATIO_WIDTH_TO_HEIGHT <= wh_ratio + EPSILON):
                         if wh_ratio < RATIO_WIDTH_TO_HEIGHT:
@@ -96,28 +110,37 @@ def main():
                             else:
                                 # remove one column at the right
                                 image = image[:, 0:width-1, ...]
-                        
+
                         height = image.shape[0]
                         width = image.shape[1]
                         wh_ratio = width / height
                         removed += 1
-                    
+
                     image_resized = misc.imresize(image, (SCALE_HEIGHT, SCALE_WIDTH))
+
+                    # save augmented image
                     filename_aug = filename.replace(".jp", "__%d.jp" % (i))
                     misc.imsave(os.path.join(write_to_dir, filename_aug), image_resized)
-            except IOError as e:
-                print("I/O error({0}): {1}".format(e.errno, e.strerror))
+            except IOError as exc:
+                # sometimes downloaded images cannot be read by imread()
+                # this should catch these cases
+                print("I/O error({0}): {1}".format(exc.errno, exc.strerror))
                 nb_errors += 1
-            
+
             nb_processed += 1
-        
+
         print("Processed %d images" % (nb_processed,))
         print("Encountered %d errors" % (nb_errors,))
         print("Finished.")
 
 def get_all_filepaths(fp_dirs):
+    """Reads all filepaths to images in provided directories.
+    Args:
+        fp_dirs The list of directories
+    Returns:
+        List of filepaths to images
+    """
     result_img = []
-    result_coords = []
     for fp_dir in fp_dirs:
         fps = [f for f in os.listdir(fp_dir) if os.path.isfile(os.path.join(fp_dir, f))]
         fps = [os.path.join(fp_dir, f) for f in fps]
